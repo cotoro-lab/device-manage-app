433ec98a0293215dc605e5784600c838333a296d
cotoro <shuta.u824@gmail.com>
2024-01-16 00:52:53 +0900

    add detail madadame

---
+++ src/features/user-master/DetailUser.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+    Box,
+    HStack, VStack,
+    Text,
+    Input,
+    Button,
+} from "@chakra-ui/react";
+
+import { Header } from "../../components/Header";
+import { BackButton } from "../../components/Button/BackButton";
+
+const page_title: string = "ユーザー詳細画面";
+
+type Props = {
+    user_id: number;
+}
+
+
+export const DetailUser = (props: Props) => {
+    return (
+        <>
+            <VStack
+                spacing={4}
+                align='stretch'
+            >
+                <Header title={page_title} />
+
+                <Box
+                    maxW="80%"
+                    mx="10%"
+                    mt="15px"
+
+                >
+                    <VStack
+                        align="stretch"
+                        maxW="60%"
+                    >
+                        <HStack justify="space-between">
+                            <Text flex="3">社員番号</Text>
+                            <Input flex="4">{props.user_id}</Input>
+                        </HStack>
+                        <HStack justify="space-between">
+                            <Text flex="3">氏名</Text>
+                            <Input flex="4"></Input>
+                        </HStack>
+                        <HStack justify="space-between">
+                            <Text flex="3">所属</Text>
+                            <Input flex="4"></Input>
+                        </HStack>
+                        <HStack justify="space-between">
+                            <Text flex="3">電話番号</Text>
+                            <Input flex="4"></Input>
+                        </HStack>
+                        <HStack justify="space-between">
+                            <Text flex="3">メールアドレス</Text>
+                            <Input flex="4"></Input>
+                        </HStack>
+                        <HStack mt="30px">
+                            <BackButton />
+                            <Button colorScheme="telegram" ml="20px">登録</Button>
+                        </HStack>
+                    </VStack>
+                </Box>
+            </VStack>
+        </>
+    );
+};
